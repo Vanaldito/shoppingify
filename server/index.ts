40a59ef6320c8c7042d2df2d02d7ad7f054caa2d
Vanaldito <barraza.fredimanuel@gmail.com>
2023-02-05 17:27:16 -0600

    chore(Root): starting the project

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("*", (_req, res) => {
+  res.status(404).send("Error 404 ~ Page not found");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log();
+  console.log(`  App running in port ${PORT}`);
+
+  if (process.env.NODE_ENV !== "production") {
+    console.log();
+    console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+  }
+});
