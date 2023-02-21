816ca73ad270649c8ec322d0c7c34708589bb5cd
Vanaldito <barraza.fredimanuel@gmail.com>
2023-02-20 22:40:56 -0600

    feat(server): add an api endpoint to delete an item from the items list

--- server/__tests__/src/routes/api/items.test.ts
+++ server/__tests__/src/routes/api/items.test.ts
@@ -746,4 +746,339 @@ describe("items.route.ts test", () => {
       error: "Internal server error",
     });
   });
+
+  it("/api/items/delete ~ Should return a status code 401 and a json error if the user auth token is invalid", async () => {
+    const response1 = await api.post("/api/items/delete").expect(401);
+
+    expect(response1.body).toEqual({
+      status: 401,
+      error: "Auth token is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", ["auth-token=something"])
+      .expect(401);
+
+    expect(response2.body).toEqual({
+      status: 401,
+      error: "Auth token is not valid",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 400 and a json error if the category is not a string", async () => {
+    const response1 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: undefined, name: "Name" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: 10, name: "Name" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 400 if the category is an empty string or a string of spaces only", async () => {
+    const response1 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "", name: "Name" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "  \n ", name: "Name" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 400 if the name is not a string", async () => {
+    const response1 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: undefined })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: {} })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 400 if the name is an empty string or a string of spaces only", async () => {
+    const response1 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: " \n \r" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 404 if the user does not exist or was deleted", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      return new Promise(resolve => resolve(undefined));
+    });
+
+    const response = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "Name" })
+      .expect(404);
+
+    expect(response.body).toEqual({
+      status: 404,
+      error: "User does not exist or was deleted",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 500 if the database throws an error when finding the user", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      throw new Error("");
+    });
+
+    const response = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "Name" })
+      .expect(500);
+
+    expect(response.body).toEqual({
+      status: 500,
+      error: "Internal server error",
+    });
+  });
+
+  it("/api/items/delete ~ Should return a status code 404 if the item does not exist in the items list (case insensitive and ignoring extra spaces)", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+              ],
+            },
+          ],
+          activeShoppingList: { name: "default--282342", list: [] },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    jest
+      .spyOn(User, "updateItems")
+      .mockImplementation(() => new Promise(resolve => resolve()));
+
+    const response1 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 2" })
+      .expect(404);
+
+    expect(response1.body).toEqual({
+      status: 404,
+      error: "Item is not in the items list",
+    });
+
+    const response2 = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "  caTegORy 1", name: "\nITem 1  " })
+      .expect(200);
+
+    expect(response2.body).toEqual({
+      status: 200,
+    });
+  });
+
+  it("/api/items/delete ~ Should remove the item", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+                { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+                { name: "Item 3", note: "Note 3", image: "https://image3.com" },
+              ],
+            },
+          ],
+          activeShoppingList: { name: "default--282342", list: [] },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    let savedItems: ItemsList = [];
+    jest.spyOn(User, "updateItems").mockImplementation((_id, items) => {
+      savedItems = items;
+
+      return new Promise(resolve => resolve());
+    });
+
+    const response = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 1" })
+      .expect(200);
+
+    expect(response.body).toEqual({
+      status: 200,
+    });
+
+    expect(savedItems).toEqual([
+      {
+        category: "Category 1",
+        items: [
+          { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+          { name: "Item 3", note: "Note 3", image: "https://image3.com" },
+        ],
+      },
+    ]);
+  });
+
+  it("/api/items/delete ~ Should remove the entire category if it is the only item in it", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+              ],
+            },
+            {
+              category: "Category 2",
+              items: [
+                { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+              ],
+            },
+          ],
+          activeShoppingList: { name: "default--282342", list: [] },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    let savedItems: ItemsList = [];
+    jest.spyOn(User, "updateItems").mockImplementation((_id, items) => {
+      savedItems = items;
+
+      return new Promise(resolve => resolve());
+    });
+
+    const response = await api
+      .post("/api/items/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 1" })
+      .expect(200);
+
+    expect(response.body).toEqual({
+      status: 200,
+    });
+
+    expect(savedItems).toEqual([
+      {
+        category: "Category 2",
+        items: [
+          { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+        ],
+      },
+    ]);
+  });
 });
--- server/src/routes/api/items.route.ts
+++ server/src/routes/api/items.route.ts
@@ -136,4 +136,79 @@ items.post("/add", async (req, res) => {
   return res.status(200).json({ status: 200 });
 });
 
+items.post("/delete", async (req, res) => {
+  const cookies = req.cookies;
+  const authToken = cookies["auth-token"];
+
+  const id = getUserIdFromToken(authToken);
+
+  if (id === undefined) {
+    return res
+      .status(401)
+      .json({ status: 401, error: "Auth token is not valid" });
+  }
+
+  const { category, name } = req.body;
+
+  const categoryIsInvalid =
+    typeof category !== "string" || category.trim() === "";
+  if (categoryIsInvalid) {
+    return res
+      .status(400)
+      .json({ status: 400, error: "Category is not valid" });
+  }
+
+  const nameIsInvalid = typeof name !== "string" || name.trim() === "";
+  if (nameIsInvalid) {
+    return res.status(400).json({ status: 400, error: "Name is not valid" });
+  }
+
+  let user;
+  try {
+    user = await User.findById(id);
+  } catch (err) {
+    console.log(err);
+
+    return res
+      .status(500)
+      .json({ status: 500, error: "Internal server error" });
+  }
+
+  if (user === undefined) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "User does not exist or was deleted" });
+  }
+
+  const items = user.items;
+
+  const categoryIndex = items.findIndex(
+    el => el.category.toLowerCase().trim() === category.toLowerCase().trim()
+  );
+  if (categoryIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "Item is not in the items list" });
+  }
+
+  const itemIndex = items[categoryIndex].items.findIndex(
+    item => item.name.toLowerCase().trim() === name.toLowerCase().trim()
+  );
+  if (itemIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "Item is not in the items list" });
+  }
+
+  if (items[categoryIndex].items.length === 1) {
+    items.splice(categoryIndex, 1);
+  } else {
+    items[categoryIndex].items.splice(itemIndex, 1);
+  }
+
+  await User.updateItems(id, items);
+
+  return res.status(200).json({ status: 200 });
+});
+
 export default items;
