7ae86be793ec3246df4f5c22db9f8eb52127a8ee
Vanaldito <barraza.fredimanuel@gmail.com>
2023-03-03 21:44:06 -0600

    feat(server): add an api endpoint to delete an item from the active shopping list

--- server/__tests__/src/routes/api/activeShoppingList.test.ts
+++ server/__tests__/src/routes/api/activeShoppingList.test.ts
@@ -626,4 +626,490 @@ describe("activeShoppingList.route.ts test", () => {
       error: "Internal server error",
     });
   });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 401 and a json error if the user auth token is invalid", async () => {
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .expect(401);
+
+    expect(response1.body).toEqual({
+      status: 401,
+      error: "Auth token is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", ["auth-token=something"])
+      .expect(401);
+
+    expect(response2.body).toEqual({
+      status: 401,
+      error: "Auth token is not valid",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 400 and a json error if the category is not a string", async () => {
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: undefined, name: "Name" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: 10, name: "Name" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 400 if the category is an empty string or a string of spaces only", async () => {
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "", name: "Name" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "  \n ", name: "Name" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Category is not valid",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 400 if the name is not a string", async () => {
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: undefined })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: {} })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 400 if the name is an empty string or a string of spaces only", async () => {
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "" })
+      .expect(400);
+
+    expect(response1.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: " \n \r" })
+      .expect(400);
+
+    expect(response2.body).toEqual({
+      status: 400,
+      error: "Name is not valid",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 404 if the user does not exist or was deleted", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      return new Promise(resolve => resolve(undefined));
+    });
+
+    const response = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "Name" })
+      .expect(404);
+
+    expect(response.body).toEqual({
+      status: 404,
+      error: "User does not exist or was deleted",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 500 if the database throws an error when finding the user", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      throw new Error("");
+    });
+
+    const response = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category", name: "Name" })
+      .expect(500);
+
+    expect(response.body).toEqual({
+      status: 500,
+      error: "Internal server error",
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 404 if the item does not exist in the shopping list (case insensitive and ignoring extra spaces)", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+              ],
+            },
+          ],
+          activeShoppingList: {
+            name: "default--282342",
+            list: [
+              {
+                category: "Category 1",
+                items: [
+                  {
+                    name: "Item 1",
+                    amount: 10,
+                    completed: false,
+                  },
+                ],
+              },
+            ],
+          },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    jest
+      .spyOn(User, "updateActiveShoppingList")
+      .mockImplementation(() => new Promise(resolve => resolve()));
+
+    const response1 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 2" })
+      .expect(404);
+
+    expect(response1.body).toEqual({
+      status: 404,
+      error: "Item is not in the shopping list",
+    });
+
+    const response2 = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "  caTegORy 1", name: "\nITem 1  " })
+      .expect(200);
+
+    expect(response2.body).toEqual({
+      status: 200,
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should remove the item", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+                { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+                { name: "Item 3", note: "Note 3", image: "https://image3.com" },
+              ],
+            },
+          ],
+          activeShoppingList: {
+            name: "default--282342",
+            list: [
+              {
+                category: "Category 1",
+                items: [
+                  {
+                    name: "Item 1",
+                    amount: 10,
+                    completed: false,
+                  },
+                  {
+                    name: "Item 2",
+                    amount: 20,
+                    completed: true,
+                  },
+                  {
+                    name: "Item 3",
+                    amount: 30,
+                    completed: false,
+                  },
+                ],
+              },
+            ],
+          },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    let savedShoppingList: ShoppingList = { name: "default--282342", list: [] };
+    jest
+      .spyOn(User, "updateActiveShoppingList")
+      .mockImplementation((_id, shoppingList) => {
+        savedShoppingList = shoppingList;
+
+        return new Promise(resolve => resolve());
+      });
+
+    const response = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 3" })
+      .expect(200);
+
+    expect(response.body).toEqual({
+      status: 200,
+    });
+
+    expect(savedShoppingList).toEqual({
+      name: "default--282342",
+      list: [
+        {
+          category: "Category 1",
+          items: [
+            {
+              name: "Item 1",
+              amount: 10,
+              completed: false,
+            },
+            {
+              name: "Item 2",
+              amount: 20,
+              completed: true,
+            },
+          ],
+        },
+      ],
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should remove the entire category if there are no more items in it", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+              ],
+            },
+            {
+              category: "Category 2",
+              items: [
+                { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+              ],
+            },
+          ],
+          activeShoppingList: {
+            name: "default--282342",
+            list: [
+              {
+                category: "Category 1",
+                items: [
+                  {
+                    name: "Item 1",
+                    amount: 5,
+                    completed: true,
+                  },
+                ],
+              },
+              {
+                category: "Category 2",
+                items: [
+                  {
+                    name: "Item 2",
+                    amount: 10,
+                    completed: false,
+                  },
+                ],
+              },
+            ],
+          },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    let savedShoppingList: ShoppingList = { name: "default--282342", list: [] };
+    jest
+      .spyOn(User, "updateActiveShoppingList")
+      .mockImplementation((_id, shoppingList) => {
+        savedShoppingList = shoppingList;
+
+        return new Promise(resolve => resolve());
+      });
+
+    const response = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 1" })
+      .expect(200);
+
+    expect(response.body).toEqual({
+      status: 200,
+    });
+
+    expect(savedShoppingList).toEqual({
+      name: "default--282342",
+      list: [
+        {
+          category: "Category 2",
+          items: [{ name: "Item 2", amount: 10, completed: false }],
+        },
+      ],
+    });
+  });
+
+  it("/api/active-shopping-list/delete ~ Should return a status code 500 if the database throws an error when updating the shopping list", async () => {
+    jest.spyOn(User, "findById").mockImplementation(() => {
+      const saltRounds = 10;
+
+      return new Promise(resolve =>
+        resolve({
+          id: 1,
+          email: "test@test.com",
+          password: bcrypt.hashSync("Password", saltRounds),
+          items: [
+            {
+              category: "Category 1",
+              items: [
+                { name: "Item 1", note: "Note 1", image: "https://image1.com" },
+              ],
+            },
+            {
+              category: "Category 2",
+              items: [
+                { name: "Item 2", note: "Note 2", image: "https://image2.com" },
+              ],
+            },
+          ],
+          activeShoppingList: {
+            name: "default--282342",
+            list: [
+              {
+                category: "Category 1",
+                items: [
+                  {
+                    name: "Item 1",
+                    amount: 5,
+                    completed: true,
+                  },
+                ],
+              },
+              {
+                category: "Category 2",
+                items: [
+                  {
+                    name: "Item 2",
+                    amount: 10,
+                    completed: false,
+                  },
+                ],
+              },
+            ],
+          },
+          shoppingHistory: [],
+        })
+      );
+    });
+
+    jest.spyOn(User, "updateActiveShoppingList").mockImplementation(() => {
+      throw new Error("");
+    });
+
+    const response = await api
+      .post("/api/active-shopping-list/delete")
+      .set("Cookie", [
+        `auth-token=${jwt.sign({ id: 1 }, env.JWT_SECRET as string)}`,
+      ])
+      .send({ category: "Category 1", name: "Item 1" })
+      .expect(500);
+
+    expect(response.body).toEqual({
+      status: 500,
+      error: "Internal server error",
+    });
+  });
 });
--- server/src/routes/api/activeShoppingList.route.ts
+++ server/src/routes/api/activeShoppingList.route.ts
@@ -129,4 +129,84 @@ activeShoppingList.post("/update", async (req, res) => {
   return res.status(200).json({ status: 200 });
 });
 
+activeShoppingList.post("/delete", async (req, res) => {
+  const cookies = req.cookies;
+  const authToken = cookies["auth-token"];
+
+  const id = getUserIdFromToken(authToken);
+
+  if (id === undefined) {
+    return res
+      .status(401)
+      .json({ status: 401, error: "Auth token is not valid" });
+  }
+
+  const { category, name } = req.body;
+
+  if (typeof category !== "string" || category.trim() === "") {
+    return res
+      .status(400)
+      .json({ status: 400, error: "Category is not valid" });
+  }
+
+  if (typeof name !== "string" || name.trim() === "") {
+    return res.status(400).json({ status: 400, error: "Name is not valid" });
+  }
+
+  let user;
+  try {
+    user = await User.findById(id);
+  } catch (err) {
+    console.log(err);
+
+    return res
+      .status(500)
+      .json({ status: 500, error: "Internal server error" });
+  }
+
+  if (user === undefined) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "User does not exist or was deleted" });
+  }
+
+  const shoppingList = user.activeShoppingList;
+
+  const categoryIndex = shoppingList.list.findIndex(
+    el => el.category.toLowerCase().trim() === category.toLowerCase().trim()
+  );
+  if (categoryIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "Item is not in the shopping list" });
+  }
+
+  const itemIndex = shoppingList.list[categoryIndex].items.findIndex(
+    item => item.name.toLowerCase().trim() === name.toLowerCase().trim()
+  );
+  if (itemIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: 404, error: "Item is not in the shopping list" });
+  }
+
+  if (shoppingList.list[categoryIndex].items.length === 1) {
+    shoppingList.list.splice(categoryIndex, 1);
+  } else {
+    shoppingList.list[categoryIndex].items.splice(itemIndex, 1);
+  }
+
+  try {
+    await User.updateActiveShoppingList(id, shoppingList);
+  } catch (err) {
+    console.error(err);
+
+    return res
+      .status(500)
+      .json({ status: 500, error: "Internal server error" });
+  }
+
+  return res.status(200).json({ status: 200 });
+});
+
 export default activeShoppingList;
