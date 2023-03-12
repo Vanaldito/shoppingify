2170d12e181a38e9d6a33a683e3042d68f60c6d2
Vanaldito <barraza.fredimanuel@gmail.com>
2023-03-11 20:59:37 -0600

    feat(client): use react-router-dom to set the home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components";
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+    </Routes>
+  );
 }
